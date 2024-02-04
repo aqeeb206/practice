e15859a917cbea01a1abc27b8651581b13a09e73
aqeeb206 <aqeeb206@gmail.com>
2024-02-04 18:08:39 +0530

    first commit

---
+++ aqeeb.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class aqeeb{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enetr 3 numbers please");
+        int  num1 = sc.nextInt();
+        int num2=sc.nextInt();
+        int num3=sc.nextInt();
+        int average=(num1+num2+num3)/3;
+        System.out.println(average);
+        sc.close();
+    }
+}
